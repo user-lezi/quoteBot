a31d5535f4943f24d5ea7cad84d6519014e15876
user-lezi <112383791+user-lezi@users.noreply.github.com>
2024-04-10 07:46:04 +0000

    bot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
+const { ForgeClient } = require("@tryforge/forgescript")
+
+const client = new ForgeClient({
+    intents: [
+        "GuildMessages",
+        "Guilds",
+        "MessageContent"
+    ],
+    events: [
+        "messageCreate",
+        "ready"
+    ],
+    prefixes: [
+        "qi"
+    ]
+})
 
 
+
+client.commands.add({
+  type: 'ready',
+  code: `
+  $log[Ready on $userTag[$clientID]]
+  `
+})
+
+
+
+client.login(process.env.TOKEN).catch(function () {})
 app.listen(3000, () => {
   console.log('server started');
 });
